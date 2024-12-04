3a6ec6e6bb13af4b0ff71760e4ce3157977d3f36
davidk <dkoeplinger@gmail.com>
2024-12-04 13:50:31 -0500

    matrix_from_file

--- nvl/data/Tensor.cpp
+++ nvl/data/Tensor.cpp
@@ -2,21 +2,24 @@
 
 #include <fstream>
 
+#include "nvl/data/List.h"
+#include "nvl/macros/Aliases.h"
+
 namespace nvl {
 
 Tensor<2, char> matrix_from_file(const std::string &filename) {
     std::ifstream file(filename);
 
     std::string line;
-    std::vector<std::string> lines;
+    List<std::string> lines;
     while (std::getline(file, line)) {
         lines.push_back(line);
     }
-    const int64_t rows = lines.size();
-    const int64_t cols = lines[0].size();
+    const I64 rows = lines.size();
+    const I64 cols = lines[0].size();
     Tensor<2, char> matrix({rows, cols}, '.');
-    for (size_t i = 0; i < rows; ++i) {
-        for (size_t j = 0; j < cols; ++j) {
+    for (I64 i = 0; i < rows; ++i) {
+        for (I64 j = 0; j < cols; ++j) {
             matrix[{i, j}] = lines[i][j];
         }
     }
