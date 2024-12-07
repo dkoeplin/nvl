31cfd69380ea0eef5d55dca94e8ffcbc81d3ae52
davidk <dkoeplinger@gmail.com>
2024-12-07 14:18:46 -0500

    Counter range, template

--- nvl/data/Counter.h
+++ nvl/data/Counter.h
@@ -10,16 +10,17 @@
 
 namespace nvl {
 
+template <typename T>
 class Counter {
 public:
-    using Value = List<I64>;
+    using Value = List<T>;
 
-    class iterator final : public AbstractIteratorCRTP<iterator, List<I64>> {
+    class iterator final : public AbstractIteratorCRTP<iterator, List<T>> {
     public:
-        static Iterator<List<I64>> begin(const Counter *ctr) { return make_iterator<iterator>(ctr, ctr->start_); }
-        static Iterator<List<I64>> end(const Counter *ctr) { return make_iterator<iterator>(ctr, None); }
+        static Iterator<List<T>> begin(const Counter &ctr) { return make_iterator<iterator>(ctr, ctr->start_); }
+        static Iterator<List<T>> end(const Counter &ctr) { return make_iterator<iterator>(ctr, None); }
 
-        explicit iterator(const Counter *counter, Maybe<List<I64>> iters) : ctr_(counter), iters_(std::move(iters)) {}
+        explicit iterator(const Counter &counter, Maybe<List<T>> iters) : ctr_(counter), iters_(std::move(iters)) {}
 
         void increment() override {
             // Increment only does something if this is not the end iterator.
@@ -39,25 +40,30 @@ public:
             }
         }
 
-        pure const List<I64> *ptr() override { return &iters_.value(); }
+        pure const List<T> *ptr() override { return &iters_.value(); }
 
-        pure bool operator==(const iterator &rhs) const override { return *ctr_ == *rhs.ctr_ && iters_ == rhs.iters_; }
+        pure bool operator==(const iterator &rhs) const override { return ctr_ == rhs.ctr_ && iters_ == rhs.iters_; }
 
     private:
-        const Counter *ctr_;
-        Maybe<List<I64>> iters_;
+        Counter ctr_;
+        Maybe<List<T>> iters_;
     };
 
-    explicit Counter(const nvl::List<I64> &end) : start_(end.size(), 0), end_(end), stride_(end.size(), 1) {}
+    static Range<List<T>> get(const U64 n, const T ends) {
+        return Counter(List<T>(n, 0), List<T>(n, ends), List<T>(n, 1)).range();
+    }
 
-    explicit Counter(const nvl::List<I64> &start, const nvl::List<I64> &end, const nvl::List<I64> &stride)
-        : start_(start), end_(end), stride_(stride) {}
+    static Range<List<T>> get(const List<T> &end) {
+        return Counter(List<T>(end.size(), 0), end_(end), List<T>(end.size(), 1)).range();
+    }
 
-    pure implicit operator Range<List<I64>>() const { return range(); }
+    static Range<List<T>> get(const List<T> &start, const List<T> &end, const List<T> &stride) {
+        return Counter(start, end, stride).range();
+    }
 
-    pure Iterator<List<I64>> begin() const { return iterator::begin(this); }
-    pure Iterator<List<I64>> end() const { return iterator::end(this); }
-    pure Range<List<I64>> range() const { return make_range<iterator>(this); }
+    pure Iterator<List<T>> begin() const { return iterator::begin(this); }
+    pure Iterator<List<T>> end() const { return iterator::end(this); }
+    pure Range<List<T>> range() const { return make_range<iterator>(this); }
 
     pure U64 size() const { return start_.size(); }
 
@@ -67,10 +73,13 @@ public:
     pure bool operator!=(const Counter &rhs) const { return !(*this == rhs); }
 
 private:
+    explicit Counter(const List<T> &start, const List<T> &end, const List<T> &stride)
+        : start_(start), end_(end), stride_(stride) {}
+
     friend class iterator;
-    nvl::List<I64> start_;
-    nvl::List<I64> end_;
-    nvl::List<I64> stride_;
+    List<T> start_;
+    List<T> end_;
+    List<T> stride_;
 };
 
 } // namespace nvl
